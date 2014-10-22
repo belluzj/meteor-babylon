de33cd9c4e07cc4734853c0d4861c7b35f7fed08
Warren McQuinn <warren.mcquinn@gmail.com>
2014-10-22 11:10:32 -0700

    1.0

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  name: 'warrenmcquinn:meteor-babylon',
+  summary: 'Meteor package for Babylon.js 3D engine.',
+  version: '1.0.0',
+  git: 'https://www.github.com/warrenmcquinn/meteor-babylon.git'
+});
+
+Package.onUse(function(api) {
+  api.addFiles('babylon.1.14-RC.js');
+});
+
